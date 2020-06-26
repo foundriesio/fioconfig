e6715340c39ddfccd2570fd1a3268e297e4e57cf
Andy Doan <andy@foundries.io>
2020-06-26 12:02:18 -0500

    Start of pkcs11 support
    This lets us connect to the device-gateway via TLS
    
    Signed-off-by: Andy Doan <andy@foundries.io>

--- internal/app.go
+++ internal/app.go
@@ -15,6 +15,7 @@ import (
 	"path/filepath"
 	"time"
 
+	"github.com/ThalesIgnite/crypto11"
 	toml "github.com/pelletier/go-toml"
 )
 
@@ -56,8 +57,69 @@ func tomlAssertVal(tree *toml.Tree, key string, allowed []string) string {
 	return val
 }
 
+// sota.toml has slot id's as "01". We need to turn that into []byte{1}
+func idToBytes(id string) []byte {
+	bytes := []byte(id)
+	start := -1
+	for idx, char := range bytes {
+		bytes[idx] = char - byte('0')
+		if bytes[idx] != 0 && start == -1 {
+			start = idx
+		}
+	}
+	//strip off leading 0's
+	return bytes[start:]
+}
+
 func createClientPkcs11(sota *toml.Tree) (*http.Client, CryptoHandler) {
-	panic("TODO")
+	module := tomlGet(sota, "p11.module")
+	pin := tomlGet(sota, "p11.pass")
+	pkeyId := tomlGet(sota, "p11.tls_pkey_id")
+	certId := tomlGet(sota, "p11.tls_clientcert_id")
+	caFile := tomlGet(sota, "import.tls_cacert_path")
+
+	cfg := crypto11.Config{
+		Path:       module,
+		TokenLabel: "aktualizr",
+		Pin:        pin,
+	}
+
+	ctx, err := crypto11.Configure(&cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	privKey, err := ctx.FindKeyPair(idToBytes(pkeyId), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	cert, err := ctx.FindCertificate(idToBytes(certId), nil, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if cert == nil || privKey == nil {
+		log.Fatal("Unable to load pkcs11 client cert and/or private key")
+	}
+
+	caCert, err := ioutil.ReadFile(caFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
+
+	tlsConfig := &tls.Config{
+		Certificates: []tls.Certificate{
+			tls.Certificate{
+				Certificate: [][]byte{cert.Raw},
+				PrivateKey:  privKey,
+			},
+		},
+		RootCAs: caCertPool,
+	}
+	transport := &http.Transport{TLSClientConfig: tlsConfig}
+	client := &http.Client{Timeout: time.Second * 30, Transport: transport}
+	return client, NewEciesPkcs11Handler(privKey)
 }
 
 func createClientLocal(sota *toml.Tree) (*http.Client, CryptoHandler) {
@@ -84,7 +146,7 @@ func createClientLocal(sota *toml.Tree) (*http.Client, CryptoHandler) {
 	transport := &http.Transport{TLSClientConfig: tlsConfig}
 	client := &http.Client{Timeout: time.Second * 30, Transport: transport}
 
-	if handler := NewEciesHandler(cert.PrivateKey); handler != nil {
+	if handler := NewEciesLocalHandler(cert.PrivateKey); handler != nil {
 		return client, handler
 	}
 	panic("Unsupported private key")
